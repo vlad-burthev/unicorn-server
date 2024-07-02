9fe4701743330eb7773d84b75d2119d1ac729b5b
vlad-burthev <burthev04@gmail.com>
2024-07-02 15:08:00 +0300

    modified user validator

--- dto/userDto.js
+++ dto/userDto.js
@@ -2,5 +2,9 @@ import { body } from "express-validator";
 
 export const userValidator = [
   body("email").isEmail().withMessage("Invalid email"),
-  body("password").notEmpty().withMessage("Password cannot be empty"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password cannot be empty")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
 ];
